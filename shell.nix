5c140c57750d8b83e1eb7c6ccbec1fb224b1ec02
Alvar Penning <post@0x21.biz>
2020-09-13 22:55:35 +0200

    Move Nix foo to ./contrib/nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ config ? {}, overlays ? [], pkgs ? import ./nix { inherit config overlays; } }:
+{ config ? {}, overlays ? [], pkgs ? import ./contrib/nix { inherit config overlays; } }:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
